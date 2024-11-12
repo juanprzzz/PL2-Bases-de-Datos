55173f26cd7355a75bd14511949900ca812a891b
juanprzzz <juan.perezresa@gmail.com>
2024-11-12 12:08:09 +0100

    Solo quedarían consultas

--- prueba2.sql
+++ prueba2.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS disco( ------------------------corregido
     nombre_grupo TEXT,
 
     titulo_disco TEXT,
-    anio_publicacion SMALLINT,
+    anio_publicacion TEXT,
     url_portada TEXT,
     CONSTRAINT disco_pk PRIMARY KEY (titulo_disco,anio_publicacion),
     CONSTRAINT disco_fk FOREIGN KEY (nombre_grupo) REFERENCES grupo(nombre_grupo) MATCH FULL
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS disco( ------------------------corregido
 
 CREATE TABLE IF NOT EXISTS genero( -----------------------corregido
     titulo_disco TEXT,
-    anio_publicacion SMALLINT,
+    anio_publicacion TEXT,
 
     genero TEXT,
     CONSTRAINT genero_pk PRIMARY KEY (genero,titulo_disco,anio_publicacion),
@@ -52,11 +52,11 @@ CREATE TABLE IF NOT EXISTS genero( -----------------------corregido
 
 CREATE TABLE IF NOT EXISTS edicion(-----------------------corregido
     titulo_disco TEXT,
-    anio_publicacion SMALLINT,
+    anio_publicacion TEXT,
 
     formato TEXT,
     pais text,
-    anio_edicion SMALLINT,
+    anio_edicion TEXT,
     CONSTRAINT edicion_pk PRIMARY KEY (formato,anio_edicion,pais), --titulo_disco,anio_publicacion
     CONSTRAINT edicion_fk FOREIGN KEY (titulo_disco,anio_publicacion) REFERENCES disco(titulo_disco,anio_publicacion) MATCH FULL
     ON DELETE RESTRICT ON UPDATE CASCADE  
@@ -64,10 +64,10 @@ CREATE TABLE IF NOT EXISTS edicion(-----------------------corregido
 
 CREATE TABLE IF NOT EXISTS cancion(-----------------------corregido
     titulo_disco TEXT,
-    anio_publicacion SMALLINT,
+    anio_publicacion TEXT,
     
     titulo_cancion TEXT,
-    duracion SMALLINT,
+    duracion TIME,
     CONSTRAINT cancion_pk PRIMARY KEY (titulo_cancion,titulo_disco,anio_publicacion), ----debil identificativa
     CONSTRAINT cancion_fk FOREIGN KEY (titulo_disco,anio_publicacion) REFERENCES disco(titulo_disco,anio_publicacion)MATCH FULL
     ON DELETE RESTRICT ON UPDATE CASCADE   
@@ -86,7 +86,7 @@ CREATE TABLE IF NOT EXISTS usuario( --------corregido
 
 CREATE TABLE IF NOT EXISTS desea( --disco-usuario --corregido
     titulo_disco TEXT,
-    anio_publicacion SMALLINT,
+    anio_publicacion TEXT,
     nombre_usuario TEXT, 
     CONSTRAINT desea_pk PRIMARY KEY (titulo_disco,anio_publicacion,nombre_usuario),
     CONSTRAINT desea_disco_fk FOREIGN KEY (titulo_disco,anio_publicacion) REFERENCES disco(titulo_disco,anio_publicacion)MATCH FULL
@@ -98,7 +98,7 @@ CREATE TABLE IF NOT EXISTS desea( --disco-usuario --corregido
 CREATE TABLE IF NOT EXISTS tiene( --usuario-ediciones ---Pero no sobra?
     formato TEXT,
     pais TEXT,
-    anio_edicion SMALLINT,
+    anio_edicion TEXT,
     nombre_usuario TEXT,
     estado TEXT,  
     CONSTRAINT tiene_pk PRIMARY KEY (formato,pais,anio_edicion,nombre_usuario),
@@ -113,7 +113,7 @@ CREATE TABLE IF NOT EXISTS tiene( --usuario-ediciones ---Pero no sobra?
 CREATE TABLE IF NOT EXISTS discoscsv(
     idDisco TEXT,
     NombreDisco TEXT,
-    fechaLanzamiento TEXT,
+    añoLanzamiento TEXT,
     idGrupo TEXT,
     NombreGrupo TEXT,
     urlGrupo TEXT,
@@ -169,34 +169,36 @@ FROM discoscsv;
 ---WHERE idGrupo IS NOT NULL;        no necesario creo
 \echo 'grupo hecho'
 
-INSERT INTO disco (titulo_disco, anio_publicacion, nombre_grupo)
+INSERT INTO disco (titulo_disco, anio_publicacion, nombre_grupo, url_portada)
 SELECT DISTINCT NombreDisco, 
-       CAST(fechaLanzamiento AS SMALLINT),  --cast en postgresql== fechaLanzamiento::SMALLINT
-       NombreGrupo
-FROM discoscsv
-WHERE NombreDisco IS NOT NULL AND fechaLanzamiento IS NOT NULL;
+        añoLanzamiento,  --cast en postgresql== fechaLanzamiento::SMALLINT
+       NombreGrupo,
+       urlPortada
+FROM discoscsv;
 \echo 'disco hecho'
 
 --descomponer genero en varias filas
 INSERT INTO genero (titulo_disco, anio_publicacion, genero)
 SELECT DISTINCT NombreDisco,
-       CAST(fechaLanzamiento AS SMALLINT),
-       unnest(string_to_array(regexp_replace(genero, "\[ | \] | \' ", '', 'g'),','))
+       añoLanzamiento,
+       regexp_split_to_table(
+           regexp_replace(trim(both '[]' from generos), '''', '', 'g'),  -- Elimina las comillas simples
+           '\s*,\s*'  -- Divide en filas usando la coma (con espacios opcionales alrededor)
+       )
        --(replace(generos, "'",''), ',')  ---quizas con '''' en vez de "'"?
        --unnest divide en varias filas
        --string to array (...,',') divide la cadena en un array de varios elementos tipo TEXT con la coma como delimitador 
-FROM discoscsv
-WHERE generos IS NOT NULL;
+FROM discoscsv;
 \echo 'genero hecho'
 
 INSERT INTO edicion (titulo_disco, anio_publicacion, formato, pais, anio_edicion)
-SELECT DISTINCT NombreDisco,
-       CAST(fechaLanzamiento AS SMALLINT),
-       formato,
-       paisEdicion,
-       CAST(añoEdicion AS SMALLINT) -- LO de null ya está configurado en COPY
-FROM edicionescsv
-WHERE formato IS NOT NULL AND paisEdicion IS NOT NULL;
+SELECT DISTINCT disco.NombreDisco,
+       disco.añoLanzamiento,
+       edicion.formato,
+       edicion.paisEdicion,
+       edicion.añoEdicion -- LO de null ya está configurado en COPY
+FROM discoscsv disco JOIN edicionescsv edicion ON disco.idDisco = edicion.idDisco
+ON CONFLICT (formato, pais, anio_edicion) DO NOTHING;
 \echo 'edicion hecho'
 
 ----falta cancion, join?
@@ -207,41 +209,75 @@ SELECT  DISTINCT nombreUsuario,
        nombreCompleto,
        email,
        passwd
-FROM usuarioscsv
-WHERE nombreUsuario IS NOT NULL;
+FROM usuarioscsv;
 \echo 'usuario hecho'
+
+
+INSERT INTO tiene (formato,pais,anio_edicion,nombre_usuario,estado)
+SELECT DISTINCT usuarioTieneEdicion.formato,
+    usuarioTieneEdicion.paisEdicion,
+    usuarioTieneEdicion.añoEdicion,
+    usuarioTieneEdicion.nombreUsuario,
+    usuarioTieneEdicion.estado
+FROM usuarioTieneEdicion JOIN usuario ON usuario.nombre_usuario = usuarioTieneEdicion.nombreUsuario JOIN edicion ON (edicion.formato = usuarioTieneEdicion.formato AND edicion.anio_edicion = usuarioTieneEdicion.añoEdicion AND edicion.pais = usuarioTieneEdicion.paisEdicion)
+ON CONFLICT (formato,pais,anio_edicion, nombre_usuario) DO NOTHING;
+\echo 'tiene hecho'
 
 INSERT INTO desea (titulo_disco, anio_publicacion, nombre_usuario)
 SELECT DISTINCT tituloDisco,
-       CAST(fechaLanzamiento AS SMALLINT),
+       añoLanzamiento,
        nombreUsuario
-FROM usuarioDeseaDisco
-WHERE nombreUsuario IS NOT NULL AND tituloDisco IS NOT NULL AND fechaLanzamiento IS NOT NULL;
+FROM usuarioDeseaDisco JOIN usuario ON usuario.nombre_usuario = usuarioDeseaDisco.nombreUsuario JOIN disco ON (disco.titulo_disco= usuarioDeseaDisco.tituloDisco AND disco.anio_publicacion = usuarioDeseaDisco.añoLanzamiento);
 \echo 'desea hecho'
+
+
 INSERT INTO edicion(titulo_disco, anio_publicacion, formato, pais, anio_edicion)
-SELECT DISTINCT nombreDisco, 
-    CAST(fechaLanzamiento AS SMALLINT), 
-    formato, 
-    paisEdicion, 
-    añoEdicion
-FROM discoscsv disco JOIN edicionescsv edicion ON disco.idDisco = edicion.idDisco;
+SELECT DISTINCT disco.nombreDisco, 
+    disco.añoLanzamiento, 
+    edicion.formato, 
+    edicion.paisEdicion, 
+    edicion.añoEdicion
+FROM discoscsv disco JOIN edicionescsv edicion ON disco.idDisco = edicion.idDisco
+ON CONFLICT (formato, pais, anio_edicion) DO NOTHING;------------------------------------------------------
 
 --Esta aqui no? pero hay un problema con duración. por cierto lo de NULL; ya está puesto en el COPY, está puesto en el COPY que si encuentra "NULL" es NULL
 INSERT INTO cancion(titulo_disco, anio_publicacion, titulo_cancion, duracion)
-SELECT DISTINCT NombreDisco, 
-    CAST(fechaLanzamiento AS SMALLINT), 
-    tituloCancion, 
-    CAST(split_part(duracion, ':', 1) AS INTEGER) || ':' || CAST(split_part(duracion, ':', 2) AS INTEGER) ::TIME --PROBAR QUE FUNCIONE --Duración es de la forma 00:00 hay que pasarlo a time
-FROM discoscsv disco JOIN cancioncsv cancion ON disco.idDisco = cancion.idDisco ;--Tengo que juntar las dos tablas para conseguir los datos que quiero
+SELECT DISTINCT disco.NombreDisco, 
+    disco.añoLanzamiento, 
+    cancion.tituloCancion, 
+    MAKE_INTERVAL (
+            mins => SPLIT_PART(cancion.duracion, ':', 1)::INTEGER, 
+            secs => split_part(cancion.duracion, ':', 2)::INTEGER) ::TIME --PROBAR QUE FUNCIONE --Duración es de la forma 00:00 hay que pasarlo a time
+FROM discoscsv disco JOIN cancionescsv cancion ON disco.idDisco = cancion.idDisco
+ON CONFLICT (titulo_disco, anio_publicacion, titulo_cancion) DO NOTHING;--Tengo que juntar las dos tablas para conseguir los datos que quiero
 
 INSERT INTO usuario(nombre_usuario, nombre, email, passwd)
 SELECT DISTINCT nombreUsuario, nombreCompleto, email, passwd
-FROM usuarioscsv;
+FROM usuarioscsv
+ON CONFLICT(nombre_usuario) DO NOTHING;
 
 
 --INSERT INTO desea(titulo_disco, anio_publicacion, nombre_usuario)
 --SELECT nombreDisco, CAST(fechaLanzamiento AS SMALLINT), nombreUsuario
 --FRO
+
+SELECT * FROM cancion LIMIT 10; ----Duraciones 00:00:00
+\d cancion;
+SELECT * FROM desea LIMIT 10; ---Algunos indica año 0,en el csv original tambien, pero quizas lo pone para indicar null
+\d desea;
+SELECT * FROM disco LIMIT 10; --url portada ????
+\d disco;
+SELECT * FROM edicion LIMIT 10; 
+\d edicion;
+SELECT * FROM genero LIMIT 10; ---MAL
+\d genero;
+SELECT * FROM grupo LIMIT 10; 
+\d grupo;
+\echo "dsfsfds";
+SELECT * FROM tiene LIMIT 10; ---MAL
+\d tiene;
+SELECT * FROM usuario LIMIT 10; 
+\d usuario;
 
 
 ROLLBACK;
