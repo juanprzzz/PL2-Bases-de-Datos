ed14f3aedba73676a7110b708eb54723add19a49
cris30158 <cristinamto08@gmail.com>
2024-11-18 20:53:19 +0100

    detalles

--- prueba2.sql
+++ prueba2.sql
@@ -3,28 +3,14 @@
 SET client_encoding = 'UTF8';
 
 BEGIN;
- /*
- \echo 'creando el esquema para la base de datos de discos'
-\echo 'creando esquema temporal. para cargar temporalmente los datos del csv'
-SET search_path='nombre del esquema/s usado'
-\echo 'cargando datos'
-\echo 'insertando datos en esquema final'
 
-
-
-restricciones de tabla=claves primarias (usar CONSTRAINT nombreidentificarclave PRIMARY_KEY (atrib1,atrib2...)) y foraneas 
-
-
-
-
-*/
 --------------------------------Tablas finales----------------------------
 
-\echo 'creando el esquema para la BBDD de películas'
+\echo 'creando el esquema final para la BBDD'
 
 CREATE TABLE IF NOT EXISTS grupo(
     nombre_grupo TEXT,
-    URL TEXT, -------------------------me da miedo que este rojo
+    URL TEXT, 
     CONSTRAINT grupo_pk PRIMARY KEY (nombre_grupo)
 );
 CREATE TABLE IF NOT EXISTS disco(
@@ -83,7 +69,7 @@ CREATE TABLE IF NOT EXISTS usuario(
 
 --------------RELACIONES-----------------
 
-CREATE TABLE IF NOT EXISTS desea( --disco-usuario --corregido
+CREATE TABLE IF NOT EXISTS desea( --disco-usuario 
     titulo_disco TEXT,
     anio_publicacion TEXT,
     nombre_usuario TEXT, 
@@ -110,7 +96,7 @@ CREATE TABLE IF NOT EXISTS tiene( --usuario-ediciones
 );
 
 -----------------------------------------tablas temporales----------------------------------
-\echo 'creando un esquema temporal'
+\echo 'Creando un esquema temporal...'
 CREATE TABLE IF NOT EXISTS discoscsv(
     idDisco TEXT,
     NombreDisco TEXT,
@@ -165,13 +151,13 @@ CREATE TABLE IF NOT EXISTS usuarioTieneEdicion(
 ------------------pasamos de temporales a finales---------------------
 
 INSERT INTO grupo (nombre_grupo, URL)
-SELECT DISTINCT NombreGrupo, urlGrupo ----distinct?
+SELECT DISTINCT NombreGrupo, urlGrupo 
 FROM discoscsv;
 \echo 'grupo hecho'
 
 INSERT INTO disco (titulo_disco, anio_publicacion, nombre_grupo, url_portada)
 SELECT DISTINCT NombreDisco, 
-        añoLanzamiento,  --cast en postgresql== fechaLanzamiento::SMALLINT
+        añoLanzamiento,  
        NombreGrupo,
        urlPortada
 FROM discoscsv;
@@ -185,9 +171,6 @@ SELECT DISTINCT NombreDisco,
            regexp_replace(trim(both '[]' from generos), '''', '', 'g'),  -- Elimina las comillas simples
            '\s*,\s*'  -- Divide en filas usando la coma (con espacios opcionales alrededor)
        )
-       --(replace(generos, "'",''), ',')  ---quizas con '''' en vez de "'"?
-       --unnest divide en varias filas
-       --string to array (...,',') divide la cadena en un array de varios elementos tipo TEXT con la coma como delimitador 
 FROM discoscsv;
 \echo 'genero hecho'
 
@@ -196,12 +179,11 @@ SELECT DISTINCT disco.NombreDisco,
        disco.añoLanzamiento,
        edicion.formato,
        edicion.paisEdicion,
-       edicion.añoEdicion -- LO de null ya está configurado en COPY
+       edicion.añoEdicion 
 FROM discoscsv disco JOIN edicionescsv edicion ON disco.idDisco = edicion.idDisco
 ON CONFLICT (titulo_disco, anio_publicacion, formato, pais, anio_edicion) DO NOTHING;
 \echo 'edicion hecho'
 
-----falta cancion, join?
 
 INSERT INTO usuario (nombre_usuario, nombre, email, passwd)
 SELECT  DISTINCT nombreUsuario,
@@ -239,7 +221,6 @@ FROM usuarioDeseaDisco JOIN usuario ON usuario.nombre_usuario = usuarioDeseaDisc
 \echo 'desea hecho'
 
 
---Esta aqui no? pero hay un problema con duración. por cierto lo de NULL; ya está puesto en el COPY, está puesto en el COPY que si encuentra "NULL" es NULL
 INSERT INTO cancion(titulo_disco, anio_publicacion, titulo_cancion, duracion)
 SELECT DISTINCT disco.NombreDisco, 
     disco.añoLanzamiento, 
@@ -270,11 +251,11 @@ SELECT * FROM tiene LIMIT 10;
 SELECT * FROM usuario LIMIT 10; 
 \d usuario;
 
--------------------------CONSULTAS-------------------------
+
 \echo '-----------------------MOSTRANDO CONSULTAS--------------------'
 
 \echo 'Consulta 1'
---CONSULTA 1 ---REVISADO
+---REVISADO
 --1. Mostrar los discos que tengan más de 5 canciones. Construir la expresión equivalente en álgebra relacional.
 SELECT cancion.titulo_disco
 FROM disco JOIN cancion ON disco.titulo_disco = cancion.titulo_disco AND disco.anio_publicacion = cancion.anio_publicacion---faltaria aniopublicacion
@@ -294,31 +275,21 @@ JOIN tiene ON (
     edicion.anio_publicacion = tiene.anio_publicacion
 )
 JOIN usuario ON tiene.nombre_usuario = usuario.nombre_usuario
-WHERE usuario.nombre_usuario = 'juangomez'
+WHERE usuario.nombre = 'Juan García Gómez' --usuario.nombre_usuario = 'juangomez' 
 LIMIT 10;
 \echo 'Consulta 3' 
 --revisar! -------------------------------------------------------------
 --3. Disco con mayor duración de la colección. Construir la expresión equivalente en álgebra relacional.
-/*
-SELECT  d.titulo_disco, --suponemos que por disco se refiere solo a la pk y duracion para comprobar
-        d.anio_publicacion, 
-        SUM(EXTRACT(EPOCH FROM c.duracion)) / 60 AS duracion_total --SUM(c.duracion) AS duracion_total
-FROM disco d JOIN cancion c ON d.titulo_disco = c.titulo_disco AND d.anio_publicacion = c.anio_publicacion
-WHERE c.duracion IS NOT NULL --si no pones esto no muestra la duracion del mayor (hace cosa rara.probar. muestra back to the drawing room 2015)
-GROUP BY d.titulo_disco, d.anio_publicacion --Como cada disco tiene varias canciones, necesitamos agrupar todas las canciones del mismo disco para poder sumar sus duraciones. Sin GROUP BY, el SUM(c.duracion) intentaría sumar todas las duraciones en una única cifra sin diferenciar los discos
-ORDER BY duracion_total desc
-LIMIT 1;
-*/
----FORMA MÁS SENCILLA
+
 SELECT c.titulo_disco, c.duracion
 FROM cancion c
 WHERE c.duracion = (SELECT MAX(c.duracion)
                     FROM cancion c);
 
-\echo 'Si queremos la mayor duración de cada disco'
+/*\echo 'Si queremos la mayor duración de cada disco'
 SELECT c.titulo_disco, MAX(c.duracion)
 FROM cancion c
-GROUP BY c.titulo_disco;
+GROUP BY c.titulo_disco;*/
 
 \echo 'Consulta 4'
 --4. De los discos que tiene en su lista de deseos el usuario Juan García Gómez, indicar el nombre de los grupos musicales que los interpretan.
@@ -327,7 +298,7 @@ SELECT  d.titulo_disco,
         d.nombre_grupo
 FROM usuario u JOIN desea ds ON u.nombre_usuario=ds.nombre_usuario
     JOIN disco d ON ds.titulo_disco=d.titulo_disco AND ds.anio_publicacion=d.anio_publicacion
-WHERE u.nombre_usuario='juangomez'
+WHERE u.nombre= 'Juan García Gómez'--u.nombre_usuario='juangomez'
 LIMIT 10;
 
 \echo 'Consulta 5' --REVISADO (QUITAR LIMIT) ¿Cómo puede salir antes una edición que un disco?
@@ -371,8 +342,7 @@ LIMIT 10;
 SELECT  d.titulo_disco, 
         d.anio_publicacion,
         e.anio_edicion, --sobra, debug
-        SUM(EXTRACT(EPOCH FROM c.duracion)) / 60 AS duracion_total --duracion en minutos . REVISAR?????????????????? 
-        --SUM(c.duracion) AS duracion_total
+        SUM(EXTRACT(EPOCH FROM c.duracion)) / 60 AS duracion_total --duracion en minutos 
 FROM disco d JOIN edicion e ON e.titulo_disco=d.titulo_disco AND e.anio_publicacion=d.anio_publicacion
     JOIN cancion c ON d.titulo_disco = c.titulo_disco AND d.anio_publicacion = c.anio_publicacion
 WHERE CAST(e.anio_edicion AS INTEGER)<=2000
@@ -380,9 +350,9 @@ GROUP BY
     d.titulo_disco, d.anio_publicacion, e.anio_edicion --si no pongo esto da error
 ORDER BY e.anio_edicion desc --sobra, debug
 LIMIT 50;
-\echo 'Consulta 8' ----REVISADO (CAMBIAR NOMBRES YA QUE LORENA NO DESEABA NINGÚN DISCO DE JUAN GARCÍA GÓMEZ)
-/*
 
+\echo 'Consulta 8' ----REVISADO (NOMBRES CAMBIADOS YA QUE LORENA NO DESEABA NINGÚN DISCO DE JUAN GARCÍA GÓMEZ)
+/*
 --8. Lista de ediciones de discos deseados por el usuario Lorena Sáez Pérez que tiene el usuario Juan García Gómez
 USAMOS SUBCONSULTA TEMPORAL CON WITH PARA SIMPLIFICAR
 PRIMERO CREAMOS UNA SUBCONSULTA CON WITH PARA SABER LOS DISCOS QUE TIENE JUAN GARCÍA GÓMEZ
@@ -397,6 +367,7 @@ SELECT d.titulo_disco, d.anio_publicacion
 FROM desea d JOIN juan_gomez_tiene jg ON d.titulo_disco=jg.titulo_disco
 JOIN usuario u ON u.nombre_usuario = d.nombre_usuario
 WHERE u.nombre = 'Marta Moreno Díaz';
+
 \echo 'Consulta 9' 
 --9. Lista todas las ediciones de los discos que tiene el usuario Gómez García en un estado NM o M. Construir la expresión equivalente en álgebra relacional.
 SELECT  e.formato,
@@ -410,13 +381,12 @@ FROM edicion e JOIN tiene t ON (
     e.anio_edicion = t.anio_edicion AND 
     e.titulo_disco = t.titulo_disco AND 
     e.anio_publicacion = t.anio_publicacion
-)
-WHERE t.nombre_usuario='juangomez' AND t.estado IN ('NM', 'M') --AND (t.estado='NM' OR t.estado='M')
+) JOIN usuario u ON u.nombre_usuario=t.nombre_usuario
+WHERE u.nombre = 'Juan García Gómez' AND t.estado IN ('NM', 'M') --AND (t.estado='NM' OR t.estado='M') --t.nombre_usuario='juangomez'
 LIMIT 10;
 
 \echo 'Consulta 10'---REVISADO (HAY AÑOS 0)
 --10. Listar todos los usuarios junto al número de ediciones que tiene de todos los discos junto al año de lanzamiento de su disco más antiguo, el año de lanzamiento de su disco más nuevo, y el año medio de todos sus discos de su colección
-
 SELECT u.nombre, COUNT(t.titulo_disco) AS Nº_ediciones, MIN(t.anio_publicacion) AS disco_más_antiguo, MAX(t.anio_publicacion) AS disco_más_nuevo, CAST(AVG(CAST(t.anio_publicacion AS SMALLINT))AS SMALLINT) AS media_años
 FROM usuario u JOIN tiene t ON u.nombre_usuario = t.nombre_usuario
 GROUP BY u.nombre;
@@ -431,7 +401,7 @@ HAVING
     COUNT(e.formato) > 5;
 
 \echo 'Consulta 12'
---12. Lista el usuario que más discos, contando todas sus ediciones tiene en la base de datos
+--12. Lista el usuario (o usuarios) que más discos, contando todas sus ediciones tiene en la base de datos
 /*
 SELECT  t.nombre_usuario,
         COUNT(*) AS total_ediciones
@@ -441,8 +411,6 @@ ORDER BY total_ediciones desc
 LIMIT 1;--NO VALE CON LIMIT (LO DIJO EN CLASE)
 */
 
-
----REVISADO PERO, SI SON DOS LOS CUALES TIENEN MÁS DISCOS, MUESTRA ESOS DOS (LO CUAL ME PARECE CORRECTO)(DETALLE MENOR)
 WITH total_ediciones AS(
     SELECT t.nombre_usuario, COUNT(*) AS total_ediciones
     FROM tiene t
